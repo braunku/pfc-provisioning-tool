4f56659dd3e02cc12dc7c5a9df4a4953a5f05bfe
Kurt Braun <42878050+braunku@users.noreply.github.com>
2021-07-21 16:06:28 -0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -10,6 +10,8 @@ show_menu(){
     printf "${menu} ${number} 2)${menu} Disable PLC Runtime ${normal}\n"
     printf "${menu} ${number} 3)${menu} Disable OPC-UA & IO-Check Services${normal}\n"
     printf "${menu} ${number} 4)${menu} Install KBUS Daemon ${normal}\n"
+    printf "${menu} ${number} 5)${menu} Install KBUS Daemon ${normal}\n"
+
     printf "${menu} ${number} 9)${menu} Restart PLC ${normal}\n"
     printf "${menu}*********************************************${normal}\n"
     printf "Please enter a menu option and enter or ${fgred}x to exit. ${normal}"
@@ -21,6 +23,24 @@ option_picked(){
     normal=`echo "\033[00;00m"` # normal white
     message=${@:-"${normal}Error: No message passed"}
     printf "${msgcolor}${message}${normal}\n"
+}
+
+show_container_menu(){
+    normal=`echo "\033[m"`
+    menu=`echo "\033[36m"` #blue
+    number=`echo "\033[33m"` #yellow
+    bgred=`echo "\033[41m"`
+    fgred=`echo "\033[31m"`
+    printf "\n${menu}********* Docker Containers ***********${normal}\n"
+    printf "${menu} ${number} a)${menu} Install Node-RED ${normal}\n"
+    printf "${menu} ${number} b)${menu} Install Mosquitto ${normal}\n"
+    printf "${menu} ${number} c${menu} Install KBUS Modbus ${normal}\n"
+    printf "${menu} ${number} d)${menu} Install Grafana ${normal}\n"
+    printf "${menu} ${number} e)${menu} Install InfluxDB ${normal}\n"
+    printf "${menu} ${number} 8)${menu} Main Menu ${normal}\n"
+    printf "${menu}*********************************************${normal}\n"
+    printf "Please enter a menu option and enter or ${fgred}x to exit. ${normal}"
+    read opt
 }
 
 clear
@@ -62,6 +82,15 @@ while [ $opt != '' ]
             sh kbus-daemon-installer-main/installer.sh
             printf "KBUS Daemon Installed";
             show_menu;
+        ;;
+
+        5) clear; # Docker sub-menu
+            option_picked "Option 5 Picked - Install Containers";
+            printf "Select Container";
+            show_docker_menu;
+        ;;
+        8) clear; # Return to main menu
+            show_dmenu;
         ;;
         9) clear;
             option_picked "Option 9 Picked - Rebooting";
