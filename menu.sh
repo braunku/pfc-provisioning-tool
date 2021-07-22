d668307cc3f2d6fd0c38b5425efcf9080105a9b6
Kurt Braun <42878050+braunku@users.noreply.github.com>
2021-07-22 09:00:53 -0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -91,7 +91,7 @@ while [ $opt != '' ]
         6) clear;
             option_picked "Option 6 Picked - Install DataPlotterApp";
             wget https://github.com/braunku/pfc-provisioning-tool/raw/main/install-dataplotter_2.4_armhf.ipk;
-            opkg install-dataplotter_2.4_armhf.ipk; 
+            opkg install install-dataplotter_2.4_armhf.ipk; 
             rm install-dataplotter_2.4_armhf.ipk;
             printf "DataPlotterApp 2.4 Installed.";
             show_menu;
