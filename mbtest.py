b886bc26edd10a21e0f98ea2e57ff9c38f51a9d1
Kurt Braun <42878050+braunku@users.noreply.github.com>
2024-06-13 15:16:34 -0700

    Update mbtest.py

--- mbtest.py
+++ mbtest.py
@@ -1,5 +1,5 @@
 from pymodbus.client import ModbusTcpClient
-client = ModbusTcpClient('192.168.4.20')
+client = ModbusTcpClient('127.0.0.1') # localhost or Remote IO IP Address
 client.connect()
 try:
     while True:
@@ -7,14 +7,14 @@ try:
         if not result.isError():
             value = result.registers[0]
             print(f"Read value: {value}")
-
             # Increment the value (circular behavior)
             new_value = (value + 1) % 256
-
             # Write the new value back
             client.write_register(address=512, value=new_value, unit=1)
             print(f"Write operation successful. New value: {new_value}")
         else:
             print("Error reading registers")
+# result = client.read_holding_registers(address=0, count=1, slave=1) # get information from IO
+# print(result) # use information
 finally:
     client.close()
