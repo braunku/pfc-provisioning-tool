5f2eb5bcd3fe4ad931b8a8fa54a2f54825a599aa
Kurt Braun <42878050+braunku@users.noreply.github.com>
2024-09-24 08:46:35 -0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -220,7 +220,9 @@ while [ $opt != '' ]
             read -sp "Enter root password: " PASSWORD
             echo
             docker run -d --restart on-failure:1 --name=fw  --env PASSWORD="$PASSWORD" --env SERVICE_NAME=fw --env MAX_UPDATE_RETRYS=5 --env CERT_NAME=ca.crt --env FILEPATH=/etc/docker/ --env FILE=daemon.json wagoautomation/fw-update-pfc200:04.04.03-26
-            printf "Please wait 15 minutes.  Do not select any more options until after reboot.";
+            printf "Please wait 15 minutes.  Leave PLC alone until $(date -d "20 minutes" +"%Y-%m-%d %H:%M:%S")";
+            sleep 10
+            /etc/config_tools/fwupdate status
             exit;
         ;;   
         v) clear;
@@ -229,7 +231,9 @@ while [ $opt != '' ]
             read -sp "Enter root password: " PASSWORD
             echo
             docker run -d --restart on-failure:1 --name=fw  --env PASSWORD="$PASSWORD" --env SERVICE_NAME=fw --env MAX_UPDATE_RETRYS=5 --env CERT_NAME=ca.crt --env FILEPATH=/etc/docker/ --env FILE=daemon.json wagoron/fw-update-pfc:04.05.10-27
-            printf "Please wait 15 minutes.  Do not select any more options until after reboot.";
+            printf "Please wait 15 minutes.  Leave PLC alone until $(date -d "20 minutes" +"%Y-%m-%d %H:%M:%S")";
+            sleep 10
+            /etc/config_tools/fwupdate status
             exit;
         ;;  
         x) clear;
