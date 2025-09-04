114b9338204e1f4ce1d0bae5e3a2e1dd0dbcde5f
Kurt Braun <42878050+braunku@users.noreply.github.com>
2025-09-04 11:17:17 -0700

    Update mbtest.py
    changed unit to device_id

--- mbtest.py
+++ mbtest.py
@@ -3,14 +3,14 @@ client = ModbusTcpClient('127.0.0.1') # localhost or Remote IO IP Address
 client.connect()
 try:
     while True:
-        result = client.read_holding_registers(address=512, count=1, unit=1)
+        result = client.read_holding_registers(address=512, count=1, device_id=1)
         if not result.isError():
             value = result.registers[0]
             print(f"Read value: {value}")
             # Increment the value (circular behavior)
             new_value = (value + 1) % 256
             # Write the new value back
-            client.write_register(address=512, value=new_value, unit=1)
+            client.write_register(address=512, value=new_value, device_id=1)
             print(f"Write operation successful. New value: {new_value}")
         else:
             print("Error reading registers")
